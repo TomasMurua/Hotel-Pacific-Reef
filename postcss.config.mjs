845567eab9962b290f4570f1b7006c91b427307c
TomasMurua <tomaspmurua@gmail.com>
2025-10-04 00:28:51 -0300

    arreglos deployment vercel

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    "@tailwindcss/postcss": {
+      optimize: false,
+    },
   },
-}
+};
 
-export default config
+export default config;
